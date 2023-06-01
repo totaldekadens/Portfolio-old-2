600412fd85ac4ec24ca4e244b5a1b064bec64b68
totaldekadens <angelica.moberg@hotmail.com>
2023-06-01 12:58:56 +0200

    Adding keys and images

--- src/pages/projects/index.jsx
+++ src/pages/projects/index.jsx
@@ -1,32 +1,48 @@
 import Head from 'next/head'
-
 import { Card } from '@/components/Card'
 import { SimpleLayout } from '@/components/SimpleLayout'
 import { formatDate } from '@/lib/formatDate'
 import { getAllArticles } from '@/lib/getAllArticles'
+import Image from 'next/image'
+import Link from 'next/link'
 
 function Article({ article }) {
   return (
-    <article className="md:grid md:grid-cols-4 md:items-baseline">
-      <Card className="md:col-span-3">
-        <Card.Title href={`/projects/${article.slug}`}>
-          {article.title}
-        </Card.Title>
-        <Card.Eyebrow
-          as="time"
-          dateTime={article.date}
-          className="md:hidden"
-          decorate
-        >
-          {formatDate(article.date)}
-        </Card.Eyebrow>
-        <Card.Description>{article.description}</Card.Description>
-        <Card.Cta>Go to project</Card.Cta>
-      </Card>
+    <article className="flex">
+      {/* md:grid md:grid-cols-2 md:items-baseline */}
+      <Link href={`/projects/${article.slug}`} className="md:w-3/4">
+        <Card className="md:col-span-3">
+          <Image
+            src={article.image}
+            className="z-20 mb-3 block rounded-lg md:hidden"
+          />
+          <Card.Title href={`/projects/${article.slug}`}>
+            {article.title}
+          </Card.Title>
+          <Card.Eyebrow
+            as="time"
+            dateTime={article.date}
+            className="md:hidden"
+            decorate
+          >
+            {formatDate(article.date)}
+          </Card.Eyebrow>
+
+          <Card.Description>{article.description}</Card.Description>
+          <div className="flex flex-wrap gap-2">
+            {article.keys.map((key) => (
+              <div className="z-20 mt-1 rounded-full bg-zinc-200 px-2 text-xs text-zinc-500 dark:bg-zinc-800 dark:text-zinc-500">
+                {key}
+              </div>
+            ))}
+          </div>
+          <Card.Cta>Go to project</Card.Cta>
+        </Card>
+      </Link>
       <Card.Eyebrow
         as="time"
         dateTime={article.date}
-        className="mt-1 hidden md:block"
+        className="mt-1 hidden flex-1 md:block"
       >
         {formatDate(article.date)}
       </Card.Eyebrow>
